31c175dfd66317040f2ec924b9ce6c9471065300
Abdallah El-Falou <abdallah.elfalou@gmail.com>
2013-11-12 15:16:16 -0500

    Revert "Revert 8fa7be8..7dd4bfa"
    This reverts commit 74587082d5342bc9dd8e0942fd6c7c67270fea1e.

--- javascript/custom.js
+++ javascript/custom.js
@@ -2,6 +2,7 @@ var socket = io.connect('http://build.kiwiwearables.com:3000');
  
 console.log("hello");
 // Here's a change (testing Git)
+// And yet another one
  
 // Declare global variables here 
 var total_smokes = new TimeSeries(),
